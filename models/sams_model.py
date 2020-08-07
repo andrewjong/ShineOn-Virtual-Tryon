db8771d6f15501569beca1c40fdfce4ba28753fb
Andrew Jong <andrewjong87@gmail.com>
2020-08-06 23:14:54 -0700

    Add more skeleton to sams

--- models/sams_model.py
+++ models/sams_model.py
@@ -1,8 +1,10 @@
 import argparse
 
 import pytorch_lightning as pl
+from torch.nn import L1Loss
 
 from models import BaseModel
+from models.networks.vgg import VGGLoss
 
 """ Self Attentive Multi-Spade """
 
@@ -14,18 +16,35 @@ class SamsModel(BaseModel):
         return parser
 
     def __init__(self):
-        pass
+        self.generator = SamsGenerator() # make this pt lightning
 
-    def forward(self):
-        pass
+        self.multiscale_discriminator = TODO() # make this pt lightning too?
+        self.temporal_discriminator = TODO() # make this pt lightning too?
 
+        self.criterion_l1 = L1Loss()
+        self.criterion_vgg = VGGLoss()
+        self.crit_adv_multiscale
+        self.crit_adv_temporal
+
+    def forward(self, *args, **kwargs):
+        self.generator(*args, **kwargs)
+
+    def configure_optimizers(self):
+        # must do individual optimizers and schedulers per each network
+        pass
 
     def training_step(self, batch, batch_idx, optimizer_idx):
 
         if optimizer_idx == 0:
             # generator
+            outputs = self.generator(batch)
+
+            # calculate loss
             pass
         if optimizer_idx == 1:
-            # discriminator
+            # discriminator, remember to update discriminator slower
+            disc_0_outputs = self.multiscale_discriminator(batch)
+            disc_1_outputs = self.temporal_discriminator(batch)
+
+            # calculate loss
             pass
-    pass
