e53b831a77c46832f23cb39f443a95e3ea0a5bc6
Andrew Jong <andrewjong87@gmail.com>
2020-08-10 00:21:18 -0700

    I wrote the generator code myself

--- models/networks/sams/sams_generator.py
+++ models/networks/sams/sams_generator.py
@@ -1,7 +1,3 @@
-"""
-Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
-Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
-"""
 import argparse
 import logging
 from typing import List, Dict
