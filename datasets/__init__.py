0f8e9b95b802c5c002d6dae15262b41d519f1ba6
Andrew Jong <andrewjong87@gmail.com>
2020-08-15 21:51:48 -0700

    Fix length bug that gaurav found

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -54,7 +54,8 @@ class CappedDataLoader:
         datacap = self.opt.datacap
         if datacap != float("inf"):
             datacap = int(datacap)
-        return min(len(self.dataset), datacap)
+        actual_len = len(self.dataset) // self.opt.batch_size
+        return min(actual_len, datacap)
 
     def __iter__(self):
         """Return a batch of data"""
