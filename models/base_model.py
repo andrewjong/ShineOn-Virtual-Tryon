Flownet2 with PT1.6! Resampling with SAMS, UNet. Fixed Data Leakage! (#87)
* flownet works with pytorch1.6

* flownet works with pytorch1.6

* fixed visualization bugs

* flow with sams, needs testing

* update on flow

* in the middle of testing

* tested flow with sams, works! downgrade from cuda 10.1 to cuda 10.0

* remove old sams env file

* save debugging code

* flownet works with pytorch1.6

* fixed visualization bugs

* flow with sams, needs testing

* update on flow

* in the middle of testing

* tested flow with sams, works! downgrade from cuda 10.1 to cuda 10.0

* remove old sams env file

* Fixed the NAN temporal discriminator bug

split_predictions() was operating under the assumption of MultiScale
Discriminator using several NLayerDiscriminators. However our temporal
disc only uses 1 NLayerDiscriminator, so it was splitting incorrectly.

* Bug was from using the wrong variable name

* flow works with multi-frame

* added flags to determine activation function for SAMS

* flownet works with pytorch1.6

* fixed visualization bugs

* flow with sams, needs testing

* update on flow

* remove data leakage, unet only works with one frame

* unet works with multiple frames, vis works too

* cuda 10.1 works w pt1.6

* clean up

* made suggested changes on PR

Co-authored-by: Gaurav Kuppa <gaurav.kuppa@aero.org>
Co-authored-by: Andrew Jong <andrewjong87@gmail.com>

models/base_model.py
@@ -16,7 +16,7 @@ from torch.utils.tensorboard import SummaryWriter
 from datasets import find_dataset_using_name
 from datasets.tryon_dataset import TryonDataset, parse_num_channels
 from datasets.vvt_dataset import VVTDataset
-
+from datasets.n_frames_interface import maybe_combine_frames_and_channels
 logger = logging.getLogger("logger")
 
 

models/sams_model.py
@@ -13,6 +13,7 @@ from datasets.tryon_dataset import parse_num_channels, TryonDataset
 from models.base_model import BaseModel
 from models.networks.loss import VGGLoss, GANLoss
 from models.networks.sams.sams_generator import SamsGenerator
+from .flownet2_pytorch.networks.resample2d_package.resample2d import Resample2d
 from options import gan_options
 from visualization import tensor_list_for_board
 
@@ -91,6 +92,8 @@ class SamsModel(BaseModel):
         )
         self.inputs = hparams.person_inputs + hparams.cloth_inputs
         self.generator = SamsGenerator(hparams)
+        self.resample = Resample2d()
+
 
         if self.isTrain:
             init = hparams.init_type, hparams.init_variance
@@ -204,6 +207,7 @@ class SamsModel(BaseModel):
 
         # make a buffer of previous frames, also (b x N x c x h x w)
         all_generated_frames: Tensor = torch.zeros_like(batch["image"])
+        flows = torch.unbind(batch["flow"], dim=1) if self.hparams.flow_warp else None
 
         # generate previous frames before this one.
         #   for progressive training, just generate from here
@@ -211,6 +215,7 @@ class SamsModel(BaseModel):
         for fIdx in range(start_idx, self.n_frames_total):
             # Prepare data...
             # all the guidance for the current frame
+            weight_boundary = TryonDataset.RGB_CHANNELS
             labelmaps_this_frame: Dict[str, Tensor] = {
                 name: lblmap[:, fIdx, :, :, :] for name, lblmap in labelmap.items()
             }
@@ -218,9 +223,16 @@ class SamsModel(BaseModel):
                 batch, fIdx, all_generated_frames
             )
             # synthesize
-            fake_frame: Tensor = self.generator.forward(
+            out: Tensor = self.generator.forward(
                 prev_n_frames_G, prev_n_labelmaps, labelmaps_this_frame
             )
+            fake_frame = out[:, :weight_boundary, :, :]
+            weight_mask = out[:, weight_boundary:, :, :]
+
+            if self.hparams.flow_warp:
+                last_generated_frame = all_generated_frames[:, fIdx - 1, :, :, :] if fIdx > 0 else torch.zeros_like(all_generated_frames[:, fIdx, :, :, :])
+                warped_flow = self.resample(last_generated_frame, flows[fIdx].contiguous())
+                fake_frame = (1 - weight_mask) * warped_flow + weight_mask * fake_frame
             # add to buffer, but don't detach; must go through temporal discriminator
             all_generated_frames[:, fIdx, :, :, :] = fake_frame
 

models/unet_mask_model.py
@@ -58,8 +58,6 @@ class UnetMaskModel(BaseModel):
         # comment andrew: Do we need to interleave the concatenation? Or can we leave it
         #  like this? Theoretically the unet will learn where things are, so let's try
         #  simple concat for now.
-        if flows is not None:
-            assert self.hparams.n_frames_total <= 1, "flow does not support this"
 
         concat_tensor = torch.cat([person_representation, warped_cloths], 1)
         outputs = self.unet(concat_tensor)
@@ -71,6 +69,7 @@ class UnetMaskModel(BaseModel):
 
         p_rendereds = outputs[:, 0:boundary, :, :]
         m_composites = outputs[:, boundary:weight_boundary, :, :]
+
         weight_masks = (
             outputs[:, weight_boundary:, :, :] if self.hparams.flow_warp else None
         )
@@ -95,33 +94,20 @@ class UnetMaskModel(BaseModel):
             if weight_masks is not None
             else None
         )
-        p_rendereds_warped = None
 
         # only use second frame for warping
-        if flows is not None:
+        all_generated_frames = []
+        for fIdx in range(self.hparams.n_frames_total):
+            if flows is not None:
+                last_generated_frame = all_generated_frames[fIdx - 1] if fIdx > 0 else torch.zeros_like(warped_cloths_chunked[0])
+                warp_flow = self.resample(last_generated_frame, flows[fIdx].contiguous())
+                p_rendered_warp = (1 - weight_masks_chunked[fIdx]) * warp_flow + weight_masks_chunked[fIdx] *  p_rendereds_chunked[fIdx]
+            p_rendered = p_rendered_warp if flows is not None else p_rendereds_chunked[fIdx]
+            p_tryon = warped_cloths_chunked[fIdx] * m_composites_chunked[fIdx] + p_rendered * (1 - m_composites_chunked[fIdx])
+            all_generated_frames.append(p_tryon)
 
-            warped_flows = [
-                self.resample(prev_im, flows[0].contiguous())
-            ]  # what is past_frame, also not sure flows has n_frames_total
 
-            p_rendereds_warped = [
-                (1 - weight) * warp_flow + weight * p_rendered
-                for weight, warp_flow, p_rendered in zip(
-                    weight_masks_chunked, warped_flows, p_rendereds_chunked
-                )
-            ]
-
-        p_tryons = [
-            wc * mask + p * (1 - mask)
-            for wc, p, mask in zip(
-                warped_cloths_chunked,
-                p_rendereds_warped
-                if p_rendereds_warped is not None
-                else p_rendereds_chunked,
-                m_composites_chunked,
-            )
-        ]
-        p_tryons = torch.cat(p_tryons, dim=1)  # cat back to the channel dim
+        p_tryons = torch.cat(all_generated_frames, dim=1)  # cat back to the channel dim
 
         return p_rendereds, m_composites, p_tryons
 
@@ -140,10 +126,16 @@ class UnetMaskModel(BaseModel):
         self.p_rendered, self.m_composite, self.p_tryon = self.forward(
             person_inputs, cloth_inputs, flow, prev_im
         )
+        self.p_tryon = torch.chunk(self.p_tryon, self.hparams.n_frames_total, dim=1)
+        self.p_rendered = torch.chunk(self.p_rendered, self.hparams.n_frames_total, dim=1)
+        self.m_composite = torch.chunk(self.m_composite, self.hparams.n_frames_total, dim=1)
+        im = torch.chunk(im, self.hparams.n_frames_total, dim=1)
+        cm = torch.chunk(cm, self.hparams.n_frames_total, dim=1)
+
         # loss
-        loss_image_l1 = F.l1_loss(self.p_tryon, im)
-        loss_image_vgg = self.criterionVGG(self.p_tryon, im)
-        loss_mask_l1 = F.l1_loss(self.m_composite, cm)
+        loss_image_l1 = F.l1_loss(self.p_tryon[-1], im[-1])
+        loss_image_vgg = self.criterionVGG(self.p_tryon[-1], im[-1])
+        loss_mask_l1 = F.l1_loss(self.m_composite[-1], cm[-1])
         loss = loss_image_l1 + loss_image_vgg + loss_mask_l1
 
         # logging
@@ -162,6 +154,13 @@ class UnetMaskModel(BaseModel):
         result.log(f"{val_}loss/G/mask_l1", loss_mask_l1, prog_bar=True)
 
         self.prev_frame = im
+        return result
+
+    def validation_step(self, batch, idx):
+        """ Must set self.batch = batch for validation_end() to visualize the last
+        sample"""
+        self.batch = maybe_combine_frames_and_channels(self.hparams, batch)
+        result = self.training_step(batch, idx, val=True)
         return result
 
     def test_step(self, batch, batch_idx):
@@ -196,9 +195,15 @@ class UnetMaskModel(BaseModel):
         person_visuals = self.fetch_person_visuals(b)
         visuals = [
             person_visuals,
-            [b["cloth"], b["cloth_mask"] * 2 - 1, self.m_composite * 2 - 1],
-            [self.p_rendered, self.p_tryon, b["image"], b["prev_image"]],
+            [b["cloth"][:, -3:, :, :], b["cloth_mask"][:, -1:, :, :] * 2 - 1, self.m_composite[-1] * 2 - 1],
+            [self.p_rendered[-1], self.p_tryon[-1], b["image"][:, -3:, :, :], b["prev_image"][:, -3:, :, :]],
         ]
+        for list_i in range(len(visuals)):
+            for list_j in range(len(visuals[list_i])):
+                tensor = visuals[list_i][list_j]
+                if tensor.dim() == 5:
+                    tensor = torch.squeeze(tensor, 1)
+                    visuals[list_i][list_j] = tensor
         tensor = tensor_list_for_board(visuals)
         # add to experiment
         for i, img in enumerate(tensor):
@@ -219,8 +224,8 @@ class UnetMaskModel(BaseModel):
         for name in person_vis_names:
             tensor: torch.Tensor = batch[name]
             if self.hparams.n_frames_total > 1:
-                channels = tensor.shape[-3] // 2
-                tensor = tensor[:, channels:, :, :]
+                channels = tensor.shape[-3] // self.hparams.n_frames_total
+                tensor = tensor[:, -1 * channels:, :, :]
             else:
                 channels = tensor.shape[-3]
 